919670023e4775f8fcbdaa8ff21e9fa8c0ed23cb
avsuva <108402819+avsuva@users.noreply.github.com>
2022-09-20 14:12:58 +0300

    .

---
+++ PP_2_1_2_Spring_HelloWorld-master/src/main/java/App.java
@@ -0,0 +1,32 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        System.out.println("HelloWorld" + " — " + bean1.equals(bean2));
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+
+        System.out.println("Cat" + " — " + bean3.equals(bean4));
+
+
+
+
+
+
+
+    }
+}
